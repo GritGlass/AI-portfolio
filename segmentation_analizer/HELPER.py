dae6d572f8c906287ae1a1fd266c011bd0d9d347
glass <glasshan914@gmail.com>
2025-08-24 11:46:18 +0900

    streamlit main page

---
+++ segmentation_analizer/HELPER.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="HELPER",
+    page_icon="👋",
+)
+
+st.write("# 모델 분석을 도와주는 HELPER!")
+
+st.markdown(
+    """
+    ### 기능 소개
+    - Segmentation : Segmentation의 결과 분석
+"""
+)
