17d86af39bd71a8dfb894e2f2f0b2465048f140a
Yu-Ri Han <105410621+GritGlass@users.noreply.github.com>
2025-10-16 11:32:24 +0900

    Update AGVVRP.py

--- AGV_VRP/model/AGVVRP.py
+++ AGV_VRP/model/AGVVRP.py
@@ -647,18 +647,3 @@ class AGVVRPOPTIM:
         print(f'{global_solution} : {global_fitness}')     
         return global_solution
 
-# if __name__ == "__main__":
-#     dist_matrix=pd.read_pickle('E:/glass_git/AGV_VRP/data/distance_matrix.pkl').to_numpy()
-#     task=pd.read_csv('E:/glass_git/AGV_VRP/data/task_re.csv').to_numpy()
-#     agv=pd.read_csv('E:/glass_git/AGV_VRP/data/agv_re.csv').to_numpy()
-
-#     save_path='E:/glass_git/AGV_VRP/log/'
-#     # save_path=E:/glass_git/AGV_VRP/result/solution
-#     population=100
-#     global_search_iteration=10
-#     local_search_iteration=2
-#     init_params={'time_dist':0.4,'dist':0.4,'random':0.2, 'pox':0.4, 'jbx':0.4, 'mutation':0.2, 'global_drop_rate':0.2}
-
-#     agvvrp=AGVVRPOPTIM(agv,task,dist_matrix,population,global_search_iteration,local_search_iteration,init_params,save_path)
-#     best_solution=agvvrp.run()
-#     print(best_solution)
